4ee6ed26bc0f710209df6e5c9366ab3b9a69f44a
a1252765 <47286931+a1252765@users.noreply.github.com>
2019-03-19 11:22:01 -0700

    Add files via upload

--- inversa.c
+++ inversa.c
@@ -17,47 +17,75 @@ void inicializaIdentidad(Matriz *, Matriz *);
 void calculaInversa(Matriz *, Matriz *);
 void imprimeMatriz(Matriz *);
 void setMemoria(Matriz *);
+Matriz multiplicaMatriz(Matriz *, Matriz *);
+Matriz Inversa(Matriz *);
 
 int main()
 {
     Matriz matriz  = { {0,0} , NULL};
-    Matriz inversa = { {0,0} , NULL};
+    Matriz identidad = { {0,0} , NULL};
+	Matriz resultante;
 
     do{
 
-        //Asignacion de memoria a las matrices.
         printf("Ingrese dimension para Matriz Cuadrada: \n");
         scanf("%d", &matriz.dimension.renglones);
         matriz.dimension.columnas = matriz.dimension.renglones;
-        inversa.dimension.renglones = matriz.dimension.renglones;
-        inversa.dimension.columnas = 2*matriz.dimension.renglones;
+        identidad.dimension.renglones = matriz.dimension.renglones;
+        identidad.dimension.columnas = 2*matriz.dimension.renglones;
 
         if(matriz.dimension.renglones > 1){
         	setMemoria(&matriz);
-        	setMemoria(&inversa);    
+        	setMemoria(&identidad);
         }
 
         else printf("Ingrese dimension mayor a 1\n");
-        
+
     } while (matriz.dimension.renglones <= 1);
-    //Fin de asignacion.
+
+	Matriz b = {{matriz.dimension.renglones,1},NULL};
+	setMemoria(&b);
 
     captura(&matriz);
-    inicializaIdentidad(&matriz,&inversa);
-    calculaInversa(&matriz,&inversa);
-    imprimeMatriz(&inversa);
+    imprimeMatriz(&matriz);
+	captura(&b);
 
-    //Liberacion de memoria cuando termina el programa.
+    inicializaIdentidad(&matriz,&identidad);
+
+    calculaInversa(&matriz,&identidad);
+    imprimeMatriz(&matriz);
+	imprimeMatriz(&identidad);
+
+	Matriz inversa = Inversa(&identidad);
+
+	resultante = multiplicaMatriz(&inversa,&b);
+
+	imprimeMatriz(&resultante);
+	printf("Hola");
+
     free(matriz.matriz);
-    free(inversa.matriz);
+    free(identidad.matriz);
+    printf("ola");
+
     return 0;
 }
 
+Matriz Inversa(Matriz *M){
+	int i,j;
+	Matriz resultante={{M->dimension.renglones,M->dimension.renglones},NULL};
+	setMemoria(&resultante);
+
+	for(i = 0; i< M->dimension.renglones; i++)
+		for (j = 0; j < M->dimension.columnas;j++)
+				resultante.matriz[i][j] = M->matriz[i][j+M->dimension.renglones];
+	return resultante;
+}
+
 void setMemoria(Matriz *M)
-{
+{	int i;
 	M->matriz = (float**) calloc(M->dimension.renglones, sizeof(float *)) ;
 	if (M->matriz == NULL) printf("No se pudo asignar memoria a Matriz\n");
-	for (int i=0; i < M->dimension.renglones; i++)
+	for (i=0; i < M->dimension.renglones; i++)
 	{
 		M->matriz[i] = (float*) calloc(M->dimension.columnas, sizeof(float));
 		if(M->matriz[i] == NULL) printf("No se pudo asignar memoria a Matriz[%d]\n", i );
@@ -70,10 +98,7 @@ void captura(Matriz *M){
     int i, j;
 
     for(i = 0; i < M->dimension.renglones; i++){
-        for(j = 0; j < M->dimension.renglones; j++){
-            if(i==0 && j==0){
-                printf("M[%d][%d] debe ser diferente 0.\n",i,j);
-            }
+        for(j = 0; j < M->dimension.columnas; j++){
             printf("Ingrese dato para M[%d][%d]:  ",i,j);
             scanf("%f", &M->matriz[i][j]);
             printf("\n");
@@ -89,21 +114,20 @@ void inicializaIdentidad(Matriz *matriz, Matriz *inversa){
 
         for(j = 0; j < inversa->dimension.columnas; j++)
         {
-            
+
             if(j == (matriz->dimension.renglones + i))
                 inversa->matriz[i][j] = 1;
-            
+
             else if(j >= matriz->dimension.renglones)
                 inversa->matriz[i][j] = 0;
-            
+
             else
                 inversa->matriz[i][j] = matriz->matriz[i][j];
         }
 }
 
 void calculaInversa(Matriz *matriz, Matriz *inversa){
-
-    int i,j,k;
+	int i,j,k;
     float temp;
 
     for(i = 0; i < matriz->dimension.renglones; i++)
@@ -111,14 +135,14 @@ void calculaInversa(Matriz *matriz, Matriz *inversa){
         for(j = 0; j < matriz->dimension.renglones; j++){
             if(i==j){
                 temp = inversa->matriz[i][j];
-                
+
                 for(k = 0; k < inversa->dimension.columnas; k++)
-                    inversa->matriz[i][k]/=temp;   
+                    inversa->matriz[i][k]/=temp;
             }
 
             if(i!=j){
                 temp = inversa->matriz[j][i]/inversa->matriz[i][i];
-                
+
                 for(k = 0; k < inversa->dimension.columnas; k++)
                     inversa->matriz[j][k] = inversa->matriz[j][k] - temp*inversa->matriz[i][k];
             }
@@ -131,7 +155,21 @@ void imprimeMatriz(Matriz *M){
     for(i = 0; i < M->dimension.renglones; i++){
         for(j = 0; j < M->dimension.columnas; j++)
             printf("%.2f\t\t",M->matriz[i][j]);
-        
+
         printf("\n");
     }
+	printf("\n");
+}
+
+Matriz multiplicaMatriz(Matriz *m1, Matriz *m2)
+{	Matriz MatrizResultante = {{m1->dimension.renglones, m2->dimension.columnas}, NULL};
+	setMemoria(&MatrizResultante);
+
+	int i,j,k;
+		for(i=0; i < m1->dimension.renglones; i++)
+			for(j=0; j< m2->dimension.columnas; j++)
+				for(k=0; k< m1->dimension.columnas; k++)
+					MatrizResultante.matriz[i][j] += m1->matriz[i][k] * m2->matriz[k][j];
+	return MatrizResultante;
+
 }
